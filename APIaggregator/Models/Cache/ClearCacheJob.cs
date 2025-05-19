83e15813ae483752defb39f07a47dae540c231f3
Christos Galanis <christos.galanis@incadea.com>
2025-05-19 20:53:28 +0300

    Add ClearCacheJob using Quartz , use ILogger instead of Console.WriteLine() and add Serilog to write the logs to a file

---
+++ APIaggregator/Models/Cache/ClearCacheJob.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Quartz;
+
+namespace APIaggregator.Models.Cache
+{
+    public class ClearCacheJob : IJob
+    {
+        private readonly IMemoryCache _cache;
+        private readonly ILogger<ClearCacheJob> _logger;
+
+        public ClearCacheJob(IMemoryCache cache, ILogger<ClearCacheJob> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            if(_cache is MemoryCache memoryCache)
+            {
+                memoryCache.Compact(1.0);
+                _logger.LogInformation("[Quartz] Cache cleared at {Time}", DateTime.Now);
+            }
+            else
+            {
+                _logger.LogWarning("[Quartz] Cache instance is not a MemoryCache");
+            }
+
+                return Task.CompletedTask;
+        }
+    }
+}
--- APIaggregator/Program.cs
+++ APIaggregator/Program.cs
@@ -1,6 +1,9 @@
 using APIaggregator.Contracts;
+using APIaggregator.Models.Cache;
 using APIaggregator.Services;
 using Polly;
+using Quartz;
+using Serilog;
 using System.Text.Json.Serialization;
 
 
@@ -20,7 +23,24 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 builder.Services.AddMemoryCache();
+
+builder.Services.AddQuartz(q =>
+{
+    var jobKey = new JobKey("ClearCacheJob");
+    q.AddJob<ClearCacheJob>(opts => opts.WithIdentity(jobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey)
+        .WithIdentity("ClearCacheJob-trigger")
+        .WithSimpleSchedule(x => x
+            .WithIntervalInHours(24) //run once every 24 hours starting from app startup time
+            .RepeatForever()));
+        //.WithCronSchedule("0 0 0 * * ?")); //Run at 00:00:00 (midnight) every day.
+});
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
 builder.Services.AddHttpClient<IWeatherService, WeatherService>();
 builder.Services.AddHttpClient<INewsService, NewsService>(client =>
@@ -28,6 +48,13 @@ builder.Services.AddHttpClient<INewsService, NewsService>(client =>
     client.DefaultRequestHeaders.Add("User-Agent", "MyAppAggregator/1.0");
 }).AddTransientHttpErrorPolicy(p => p.RetryAsync(3));
 builder.Services.AddHttpClient<IGithubService, GithubService>();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
